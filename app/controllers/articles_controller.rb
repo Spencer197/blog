c8049e34b88ea16329dc3af64471b59509262f2e
Spencer197 <spncr197@gmail.com>
2017-01-07 20:05:17 +0900

    Add article list, navigation links, and article title validation:5.8-5.10.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,16 +1,27 @@
 class ArticlesController < ApplicationController
+  def index#Add the corresponding index action for the route inside the ArticlesController.
+    @articles = Article.all
+  end
+
   def show#Adds show action
     @article = Article.find(params[:id])# Finds the article we're interested in, passing in params[:id] to get the :id parameter from the request
   end#We also use an instance variable (prefixed with @) to hold a reference to the article object. We do this because Rails will pass all instance variables to the view.
 
   def new
+    @article = Article.new
   end
+
+  #The reason why we added @article = Article.new in the ArticlesController is that otherwise
+  #@article would be nil in our view, and calling @article.errors.any? would throw an error.
 
   def create
   @article = Article.new(article_params)#Initializes new Article model with it artibutes (article_params).
 
-  @article.save#Saves the model in the database.
+  if @article.save#Saves the model in the database.
   redirect_to @article#Redirect the user to the show action.
+  else# If @article.save fails, we need to continue showing the new form back to the user.
+    render 'new'
+  end
 end
 
 private#This method tells Rails exactly which parameters are allowed into our controller actions.  It is made private to make sure it can't be called outside its intended context
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+
+    validates :title, presence: true,
+                        length: { minimum: 4 }#ensure that all articles have a title that is at least four characters long.
+
 end
