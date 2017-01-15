83faa98ef0a357abdbef34b7ff216cfb794c898f
Spencer197 <spncr197@gmail.com>
2017-01-15 16:06:55 +0900

    Add delete comment from article functionality:Unit-8

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,16 @@ class CommentsController < ApplicationController
       redirect_to article_path(@article)
     end
 
+    def destroy
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+    end
+    #The destroy action will find the article we are looking at, locate the comment within
+    #the @article.comments collection, and then remove it from the database and send us back to the
+    #show action for the article.
+
     private
       def comment_params
         params.require(:comment).permit(:commenter, :body)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,9 @@
 class Article < ApplicationRecord
-    has_many :comments#Tells ApplicationRecord that an article may have more than one (many) comments.
+    has_many :comments, dependent: :destroy#Tells ApplicationRecord that an article may have more than one (many) comments.
     validates :title, presence: true,
-                        length: { minimum: 4 }#ensure that all articles have a title that is at least four characters long.
-
+                        length: { minimum: 4 }#Ensures that all articles have a title that is at least four characters long.
 end
-#These two declarations(has_many in article.rb & belongs_to in comment.rb) enable a good bit of automatic behavior. 
+#These two declarations(has_many in article.rb & belongs_to in comment.rb) enable a good bit of automatic behavior.
 #For example, if you have an instance variable @article containing an article,
 #you can retrieve all the comments belonging to that article as an array using @article.comments.
+#We use a 'dependent option' in line 2 to remove comments associated with the comment we deleted(destroyed). (Added in Unit-8)
